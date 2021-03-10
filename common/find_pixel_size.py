f1d9d6f9a0247935c8c4a9c8a4390ebf929acd79
Tabatabai <tabatabai@20110001-y07f5.seattleu.edu>
2021-03-10 15:18:32 -0800

    find_pixel_size automatically writes file

--- common/find_pixel_size.py
+++ common/find_pixel_size.py
@@ -21,14 +21,30 @@ import pandas as pd
 # filename = '/Volumes/storage/pasha/2020/201210/linescan_rulerOnAcrylicSheetOnFiberBoardOnRecycleBin.csv'
 
 
-def main(filename):
-    df = pd.read_csv(filename) 
+def main(filepath,unit,unit_str):
+    """Calculates the pixel size for a given image based on a ruler
+    inputs: filepath- full path to file containing ruler information
+                        (ex: '/Volumes/storage/pasha/2021/210226' )
+            unit - number corresponding to repeating distance
+                        (ex: 0.125 for 1/8 ticks)
+            unit_str - string of actual unit value corresponding to unit
+                        (ex: 'inch')
+                        
+    outputs: saves file 'pixel_conversion.txt' in filepath
+            saved file contains conversion information
+                 
+    Example Execution
+    main('/Volumes/storage/pasha/2021/210226',.125,'inch')
+                        
+    """
+    fullfile = filepath + '/ruler_linescan.csv' #expects this naming for filename
+    df = pd.read_csv(fullfile) 
     
     # Plot raw data
     ax1 = plt.subplot(1,2,1)
     plt.plot(df['Distance_(pixels)'], df.Gray_Value) 
     ax1.set_xlim(200,300)
-    ax1.set_ylim(60,90)
+    #ax1.set_ylim(60,90)
     ax1.set_ylabel('Pixel Gray Value')
     ax1.set_xlabel('Pixel Position')
     
@@ -54,8 +70,26 @@ def main(filename):
     frequency = xf[indx]
     repeating_distance = 1/frequency
     
+    # Creates value for cm_per_pixel depending on the unit you used
+    if unit_str == 'inch':
+        cm_per_pixel = unit/repeating_distance * 2.54
+    elif unit_str == 'cm':
+        cm_per_pixel = unit/repeating_distance
+    else:
+        print('You have a unit that your code wasnt ready for')
+    
+    print('Max Frequency = ', str(frequency))
     print('Repeating distance = ', str(repeating_distance), ' pixels')
-
+    file1 = open(filepath + '/pixel_conversion.txt',"w") 
+    
+    #write required informational file
+    L = ['The repeating distance is: ' + str(repeating_distance) + ' pixels\n',
+         'The repeating unit in my image is: ' + str(unit) + unit_str + '\n',
+         'Therefore, the pixel conversion is: ' + str(unit/repeating_distance) + ' ' +unit_str +' per pixel\n',
+         'For trex, the cm_per_pixel parameter is: ' + str(cm_per_pixel) + '\n']  
+    file1.writelines(L) 
+    file1.close() #to change file access modes 
+  
     
 # inches_per_pixel = 1/(8*repeating_distance) # this is where 1/8 inch increment comes in
 # pixels_per_inch = 1/inches_per_pixel
@@ -66,6 +100,6 @@ def main(filename):
 # print('Pixels per inch = ', str(pixels_per_inch))
 
 
-
-
+# Example Execution
+# main('/Volumes/storage/pasha/2021/210226',.125,'inch')
 
